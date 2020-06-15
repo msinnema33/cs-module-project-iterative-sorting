cbd6ddb3a78ce714b3400bf4c1748e3a07d2e195
Mike Sinnema <msinnema33@gmail.com>
2020-06-15 09:20:49 -0700

    sorting is working including the stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,16 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
+        # find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
-
-        # TO-DO: swap
-        # Your code here
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+        # swap
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -18,7 +18,15 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    n = len(arr)
+    # Loop through your array
+    for i in range(n):
+        for j in range(0, n-i-1):
+    # Compare each element to its neighbor
+            if arr[j] > arr[j + 1]:
+    # If elements in wrong position (relative to each other, swap them)
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    # If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1
 
     return arr
 
@@ -39,8 +47,21 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
+def count_sort(arr, maximum=None):
     # Your code here
-
+    count_list = [0]*201 # sets 50 buckets +1
+    # loop through array and increment if exists
+    for n in arr:
+        count_list[n] = count_list[n] + 1
+    # sorts in place and copies back to original list    
+    i = 0
+    for n in range(len(count_list) - 1):
+        while count_list[n] > 0:
+            if arr[i] < 0:
+                return ("Error, negative numbers not allowed in Count Sort")
+            arr[i] = n
+            i += 1
+            count_list[n] -= 1
+            
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # return the index if found
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    # var setup (lo, hi, mid)
+    lo = 0
+    hi = len(arr) -1
+    mid = 0
+    while lo <= hi:
+        mid = (hi + lo) // 2  ## need double // to ensure integer
+        if arr[mid] == target:
+            return mid
+        elif target > arr[mid]: # take out lower(left side), set mid to lo
+            lo = mid + 1
+        else: # take out upper(right side), set mid to hi
+            hi = mid -1
     return -1  # not found
